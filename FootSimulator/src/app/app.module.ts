c0b853f221b10aa7f08ddb1e1b2851147257c449
Abdel Malik Djendli <a.malik.djendli@gmail.com>
2023-10-26 20:16:23 +0200

    header et footer

--- FootSimulator/src/app/app.module.ts
+++ FootSimulator/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import {ApiService} from "./services/api.service";
 import {HttpClientModule} from "@angular/common/http";
 import { ApiTestComponent } from './Composants/api-test/api-test.component';
+import { HeaderComponent } from './Composants/header/header.component';
+import { FooterComponent } from './Composants/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ApiTestComponent
+    ApiTestComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
